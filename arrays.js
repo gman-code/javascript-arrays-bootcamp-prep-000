0a22763c0d2b3cdb7f8cfa0a4796dd2f8de25ecf
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-15 20:15:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,4 +24,22 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  
+    array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
